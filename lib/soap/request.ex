f60fc3c79de5c62a49921dcaf4879e73902a79c7
Thales Flores <22772984+thalesflores@users.noreply.github.com>
2023-02-08 10:05:30 -0500

    Handle HTTPoison redirects
    HTTPoison only handles redirects to the status `303` in `POST` requests.
    When the status code is different it returns a `HTTPoison.MaybeRedirect`
    with the link to the redirect. On this commit we are handle any
    redirects from the HTTPoison.

--- lib/soap/request.ex
+++ lib/soap/request.ex
@@ -17,11 +17,20 @@ defmodule Soap.Request do
     request_headers = Headers.build(wsdl, operation, request_headers)
     body = Params.build_body(wsdl, operation, params, soap_headers)
 
-    get_http_client().post(url, body, request_headers, opts)
+    case execute_request(url, body, request_headers, opts) do
+      {:ok, %HTTPoison.MaybeRedirect{redirect_url: redirect_url}} ->
+        execute_request(redirect_url, body, request_headers, opts)
+
+      response ->
+        response
+    end
   end
 
   def call(wsdl, operation, params, request_headers, opts),
     do: call(wsdl, operation, {%{}, params}, request_headers, opts)
+
+  defp execute_request(url, body, request_headers, opts),
+    do: get_http_client().post(url, body, request_headers, opts)
 
   @spec get_http_client() :: HTTPoison.Base
   def get_http_client do
